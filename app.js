630810e862d517e1f83e7be92c6c0ff4d2147c4e
Weijie Tan <twj3454@hotmail.com>
2023-07-17 13:09:27 -0400

    added custom style to styles.css; set static variable for express to host the entire dir

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname, { // host the whole directory
+    extensions: ["html", "htm", "gif", "png", "css"],
+}));
 const https = require("https");
 
+// send login home page when client access the homepage
 app.get("/", (req, res)=>{
     homepagePath = __dirname + "/signup.html";
     res.sendFile(homepagePath, (err)=>{
@@ -12,6 +16,16 @@ app.get("/", (req, res)=>{
         } else {}
     });
     console.log("Home route get request handled successfully!");
+});
+
+// handle post request to the home route when client submits the form with their email info
+app.post("/", (req, res)=>{
+    console.log("received post request... handling ...");
+    const clientInfo = req.body;
+    res.write("<h1> Client Email:" + clientInfo.email + "</h1>");
+    res.write("<h1> Client Password:" + clientInfo.password + "</h1>");
+    res.send();
+    console.log("Home route post request handled successfully!");
 });
 
 app.listen(3000, ()=>{
